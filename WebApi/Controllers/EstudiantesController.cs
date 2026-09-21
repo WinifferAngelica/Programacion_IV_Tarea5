9402a1b4ddc3c40e1dcb82e2a9611bc72133b5a2
WinifferAngelica <winifferhg@gmail.com>
2026-09-21 17:48:19 -0400

    Crear clase y controlador

---
+++ WebApi/Controllers/EstudiantesController.cs
@@ -0,0 +1,30 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Mvc;
+using WebApi.Data;
+
+namespace WebApi.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class EstudiantesController : ControllerBase
+{
+    private readonly DapperDB _db;
+
+    public EstudiantesController(DapperDB db)
+    {
+        _db = db;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Get()
+    {
+        using var connection = _db.CreateConnection();
+
+        var estudiantes = await connection.QueryAsync(
+            "SELECT * FROM Estudiantes"
+        );
+
+        return Ok(estudiantes);
+    }
+}
+
---
+++ WebApi/Data/DapperDB.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace WebApi.Data;
+
+public class DapperDB
+{
+    private readonly IConfiguration _configuration;
+
+    public DapperDB(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    public SqlConnection CreateConnection()
+    {
+        return new SqlConnection(
+            _configuration.GetConnectionString("DefaultConnection")
+        );
+    }
+}
