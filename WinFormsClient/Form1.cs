dd05bfa310bf0b83b97b555c3c9487a070a302ce
WinifferAngelica <winifferhg@gmail.com>
2026-09-22 08:23:01 -0400

    Corrijo carga de estudiantes en WinForms

--- WinFormsClient/Form1.cs
+++ WinFormsClient/Form1.cs
@@ -19,9 +19,23 @@ namespace WinFormsClient
 
         private async void btnCargar_Click(object sender, EventArgs e)
         {
-            var estudiantes = await _httpClient.GetFromJsonAsync<List<Estudiantes>>("https://hogwartswebapi-emfgduhhb5cpbeck.centralus-01.azurewebsites.net/api/Estudiantes");
-            dgvEstudiantes.DataSource = estudiantes;
+            btnCargar.Enabled = false;
+            try
+            {
+                var estudiantes = await _httpClient.GetFromJsonAsync<List<Estudiantes>>("https://hogwartswebapi-emfgduhhb5cpbeck.centralus-01.azurewebsites.net/api/Estudiantes");
+                dgvEstudiantes.DataSource = estudiantes;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"No se pudo cargar el listado: {ex.Message}", "Error de conexión",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                btnCargar.Enabled = true;
+            }
         }
+
 
         private void Form1_Load(object sender, EventArgs e)
         {
